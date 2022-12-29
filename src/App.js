1ebae2be84c48011abc4e03e4d3f60fd00471a45
gchaewon <chaewon@ichaewon-ui-MacBookPro.local>
2022-12-29 21:46:20 +0900

    project init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+//메인
+import MainPage from "./pages/mainpage/MainPage";
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+       <Routes>
+          <Route exact path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
