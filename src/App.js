07b21166f251c3e98e39e8b3985b5d223902c092
gchaewon <chaewon@ichaewon-ui-MacBookPro.local>
2023-01-05 02:09:33 +0900

    feat: login 페이지 추가

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 // 랜딩 페이지
 import LandingPage from "./pages/landingPage/LandingPage";
+// 로그인 페이지 
+import LoginPage from "./pages/authPage/LoginPage";
+
 function App() {
   return (
     <Router>
        <Routes>
-          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/" element={<LandingPage />} />  
+      </Routes>
+      <Routes>
+        <Route exact path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
+ 
