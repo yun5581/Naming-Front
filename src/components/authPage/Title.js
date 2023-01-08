7082d720d02025d44d5e627e16ab8ba2c08162d2
gchaewon <chaewon@ichaewon-ui-MacBookPro.local>
2023-01-08 13:41:36 +0900

    chore: 스타일 수정

--- src/components/authPage/Title.js
+++ src/components/authPage/Title.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { SF_HambakSnow } from "../Text";
-import { vw, vh, useResize } from "../SizeConvert";
+import { vw, useResize } from "../SizeConvert";
 const Title = () =>{
     useResize();
     return(
@@ -8,8 +8,8 @@ const Title = () =>{
             <Wrapper>
                 <SF_HambakSnow>
                     <p style={{fontSize: vw(26)}}>이름하여 이름하다</p>
-                    <hr style={{marginTop: vh(8)}}/>
-                    <div style={{fontSize: vw(13), marginTop:vh(20)}}>
+                    <hr style={{marginTop: "8px"}}/>
+                    <div style={{fontSize: vw(13), marginTop:"20px"}}>
                         친구들이 정의하는 '나'로
                         <br/>
                         나만의 사전을 채워보세요
@@ -25,7 +25,7 @@ export default Title;
 const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
-    margin-top:${vh(54)};
+    margin-top: 54px;
     text-align: center;
     color: var(--white);
 `
