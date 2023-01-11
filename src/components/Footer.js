63b1fae74dda73f1ba7eb2fd665ab440ffb0f043
yun5581 <lonnie513@ewhain.net>
2023-01-11 18:41:21 +0900

    fix: 푸터 크기 픽스

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import { Simonetta } from "./Text"
 import { vw, vh } from "../components/SizeConvert";
-import styled from "styled-components";
 
 const Footer = () =>{
     return (
@@ -8,7 +7,6 @@ const Footer = () =>{
             <Simonetta style={{
                 fontSize: '8px',
                 color: "#FFFFFF",
-                textAlign: 'center',
                }}>ⓒ 2022. likelion_ewha All rights reserved.</Simonetta>
         </>
     )
