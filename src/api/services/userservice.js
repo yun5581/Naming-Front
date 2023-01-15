5f0bd99809700c945612547515e3cf03f7b7dc0e
gchaewon <conjs1@naver.com>
2023-01-15 22:55:44 +0900

    feat: api 초기셋팅

---
+++ src/api/services/userservice.js
@@ -0,0 +1,30 @@
+import { http } from "../http.js";
+import axios from "axios";
+import { persistor } from "../../index";
+
+const UserService = {
+  // 로그아웃
+  logout: () => {
+    console.log("로그아웃 되었습니다.");
+    persistor.purge();
+    window.localStorage.removeItem("token");
+    window.location.href = "http://localhost:3000/"; // url 수정 필요
+  },
+
+  //로그인
+  getUser: (id, password) =>
+    axios.post("http://localhost:4000/accounts/", { // url 수정 필요
+      loginId: id,
+      password: password
+    }),
+
+  // 회원가입
+  postUser: (name,id, password) =>
+    axios.post("http://localhost:4000/accounts/signup/", {
+        firstname: name,
+        loginId: id,
+        password: password
+    })
+};
+
+export default UserService;
