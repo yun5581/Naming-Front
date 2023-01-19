479e1a6207b0d73ffe883dc8fef0ed167b2b97bb
gchaewon <conjs1@naver.com>
2023-01-19 17:16:15 +0900

    feat: redux 초기 설정

---
+++ src/redux/store.js
@@ -0,0 +1,31 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "redux";
+import { useDispatch, useSelector } from "react-redux";
+
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
+import thunk from "redux-thunk";
+import userReducer from "./userSlice";
+import dictionaryReducer from "./dictionarySlice";
+
+const reducers = combineReducers({
+  user: userReducer,
+  dictionary: dictionaryReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
+});
+
+export const useAppDispatch = () => useDispatch();
+export const useAppSelector = useSelector;
