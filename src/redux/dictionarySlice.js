5d55e384d58d4534757e5fdefce60a9e0592baae
gchaewon <conjs1@naver.com>
2023-01-25 14:59:43 +0900

    feat: redux test 코드 추가

--- src/redux/dictionarySlice.js
+++ src/redux/dictionarySlice.js
@@ -5,7 +5,6 @@ const name = "DictionarySlice";
 
 const initialState = {
     dictionaryId: "", // 사전 고유 번호
-    userId: "", // 유저 고유 번호
     title: "", // 사전 제목 (유저 이름)
     color: "", // 표지 색
     shadow: "", // 실루엣
@@ -19,26 +18,28 @@ export const dictionarySlice = createSlice({
   reducers: {
     initDictionary: state => {
       state.dictionaryId = initialState.dictionaryId;
-      state.userId = initialState.userId;
       state.title = initialState.title;
       state.color = initialState.color;
       state.shadow = initialState.shadow;
+      state.shadowColor = initialState.shadowColor;
       state.border = initialState.border;
     },
-    setDictionary: (state, action) => {
-      state.dictionaryId = action.payload.username;
-      state.userId = action.payload.userId;
+    setDictionary: (state, action)=> {
       state.title = action.payload.title;
       state.color = action.payload.color;
       state.shadow = action.payload.shadow;
-      state.boothId = action.payload.boothId;
+      state.shadowColor = action.payload.shadowColor;
+      state.border = action.payload.border;
     },
+    setDictionaryID: (state, action) => {
+      state.dictionaryId = action.payload.dictionaryId;
+    }
   },
   extraReducers: builder => {
     builder.addCase(PURGE, () => initialState);
   },
 });
 
-export const { setDictionary, initDictionary } = dictionarySlice.actions;
+export const { setDictionaryID, initDictionary } = dictionarySlice.actions;
 
 export default dictionarySlice.reducer;
