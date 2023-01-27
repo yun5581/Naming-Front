0ea927f84c2bca36971b2d1a5abce27764df6b81
gchaewon <conjs1@naver.com>
2023-01-27 22:44:04 +0900

    chore: api url 수정

--- src/api/services/userservice.js
+++ src/api/services/userservice.js
@@ -1,32 +1,37 @@
 import { http } from "../http.js";
 import axios from "axios";
 import { persistor } from "../../index";
+import { initUser } from "../../redux/userSlice.js";
+import { initDictionary } from "../../redux/dictionarySlice.js";
 
 const UserService = {
   // 로그아웃
   logout: () => {
-    console.log("로그아웃 되었습니다.");
-    persistor.purge();
+    //persistor.purge();
     window.localStorage.removeItem("token");
-    window.location.href = "http://localhost:3000/"; // url 수정 필요
+    // 유저 정보 초기화
+    initUser();
+    // 사전 정보 초기화 
+    initDictionary();
+    window.location.href = "http://localhost:3000/"; //url 수정 필요
   },
   // 회원가입
     postUser: (id,name, password) =>
-    axios.post('https://kj173456.pythonanywhere.com/accounts/signup/', { // url 수정 필요
+    axios.post('https://kj173456.pythonanywhere.com/accounts/signup/', { 
         userId: id,
         firstName: name,
         password: password
   }),
   // 로그인
   getUser: (id, password) =>
-    axios.post("https://kj173456.pythonanywhere.com/accounts/login/", { // url 수정 필요 
+    axios.post("https://kj173456.pythonanywhere.com/accounts/login/", {
       userId: id,
       password: password
   }),
 
   // 커스텀 정보 전달 
   submitCustom:  (userId, color, shadow, shadowColor, border) =>
-    axios.post("http://localhost:4000/dictionary/",{ //url 수정 필요
+    axios.post("https://kj173456.pythonanywhere.com/dictionary/",{ 
         userId: userId,
         color: color,
         shadow: shadow,
