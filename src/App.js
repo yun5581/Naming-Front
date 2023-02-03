bc46fef129ac685d7b696dc59a216dc6a2854c10
miinjoo <minjoo9903@ewhain.net>
2023-02-04 01:10:19 +0900

    feat: 검색페이지 api 연결 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React from 'react';
+import React from "react";
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	useLocation,
-} from 'react-router-dom';
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 // 랜딩 페이지
-import LandingPage from './pages/landingPage/LandingPage';
+import LandingPage from "./pages/landingPage/LandingPage";
 // 로그인 페이지
-import LoginPage from './pages/authPage/LoginPage';
+import LoginPage from "./pages/authPage/LoginPage";
 // 회원가입 페이지
-import RegisterPage from './pages/authPage/RegisterPage';
+import RegisterPage from "./pages/authPage/RegisterPage";
 // 사전 커스텀 페이지
-import CustomPage from './pages/customPage/CustomPage';
+import CustomPage from "./pages/customPage/CustomPage";
 // 가입자 홈 페이지
-import HomePage from './pages/homePage/HomePage';
+import HomePage from "./pages/homePage/HomePage";
 // 검색 페이지
-import SearchPage from './pages/searchPage/SearchPage';
+import SearchPage from "./pages/searchPage/SearchPage";
 //정의 페이지
-import DefinitionPage from './pages/definitionPage/DefinitionPage';
+import DefinitionPage from "./pages/definitionPage/DefinitionPage";
 // 방문자 랜딩 페이지
 import VisitorLandingPage from "./pages/visitorPage/VisitorLandingPage";
 // 방문자 정의 첫작성 페이지
 import VisitorFirstPage from "./pages/visitorPage/VisitorFirstPage";
 
 function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route exact path="/" element={<LandingPage />} />
-				<Route exact path="/login" element={<LoginPage />} />
-				<Route exact path="/home" element={<HomePage />} />
-				<Route exact path="/register" element={<RegisterPage />} />
-				<Route exact path="/custom" element={<CustomPage />} />
-				<Route exact path="/search" element={<SearchPage />} />
-				<Route exact path="/definition" element={<DefinitionPage />} />
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/home" element={<HomePage />} />
+        <Route exact path="/register" element={<RegisterPage />} />
+        <Route exact path="/custom" element={<CustomPage />} />
+        <Route exact path="/search" element={<SearchPage />} />
+        <Route exact path="/definition" element={<DefinitionPage />} />
         <Route exact path="/visitorlanding" element={<VisitorLandingPage />} />
-        <Route exact path="/visitorfirst" element={<VisitorFirstPage/>} />
-			</Routes>
-		</Router>
-	);
+        <Route exact path="/visitorfirst" element={<VisitorFirstPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/api/user.js
+++ src/api/user.js
@@ -25,15 +25,74 @@ export const GetUser = async (id, password) => {
     return Promise.reject(error.response.data, "로그인 실패");
   }
 };
-// 커스텀 정보 저장 
-export const SubmitCustom = async(userId, color, shadow, shadowColor, border) =>{
-  try{
-    const response = await UserService.submitCustom(userId, color, shadow, shadowColor, border);
-    return Promise.resolve(response.data)
-  } catch(error){
-      return Promise.reject(error, "커스텀 정보 저장 실패");
+// 커스텀 정보 저장
+export const SubmitCustom = async (
+  userId,
+  color,
+  shadow,
+  shadowColor,
+  border
+) => {
+  try {
+    const response = await UserService.submitCustom(
+      userId,
+      color,
+      shadow,
+      shadowColor,
+      border
+    );
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error, "커스텀 정보 저장 실패");
   }
 };
 
+//정의 삭제
+export const deleteDictionary = async (consonant, contents) => {
+  try {
+    const response = await UserService.deleteDictionary(consonant, contents);
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error, "정의 등록 실패");
+  }
+};
 
+//자음으로 시작하는 정의들 모아서 보여주기
+export const getDictionary = () => {
+  try {
+    const response = UserService.getDictionary();
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error, "정의 가져오기 실패");
+  }
+};
 
+//이름 검색 리스트 조회
+export const getNames = (keyword) => {
+  try {
+    const response = UserService.getNames(keyword);
+    return Promise.resolve(response);
+  } catch (error) {
+    return Promise.reject(error, "이름 검색 리스트 조회 실패");
+  }
+};
+
+//정의 좋아요
+export const postLike = async () => {
+  try {
+    const response = await UserService.postLike();
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error, "좋아요 실패");
+  }
+};
+
+//정의 좋아요 취소
+export const deleteLike = async () => {
+  try {
+    const response = await UserService.deleteLike();
+    return Promise.resolve(response);
+  } catch (error) {
+    return Promise.reject(error, "좋아요 취소 실패");
+  }
+};
