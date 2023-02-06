c777f2ba250b427eac6e32efe72ddae99049ae34
gchaewon <conjs1@naver.com>
2023-02-06 15:17:23 +0900

    fix: dictionaryId dispatch 문제 해결

--- src/pages/authPage/LoginPage.js
+++ src/pages/authPage/LoginPage.js
@@ -36,17 +36,18 @@ const LoginPage = () =>{
                     ID: id,
                     PW: password
                 }));
-                navigate("/home");
+                // 사전 아이디 받아오기
+                axios.get(`https://kj273456.pythonanywhere.com/dictionary/id/${res.data.user_id}`)
+                .then((res)=>{
+                    dispatch(setDictionaryID({dictionaryId: res.data.data.id}));
+                }).then(()=>{  
+                    navigate("/home");
+                    window.location.reload();
+                }).catch((error)=>{
+                    alert("사전 정보를 가져오지 못했습니다. 재로그인해주세요.");
+                    navigate(-1);
+                });
             }
-        }).then(()=>{
-            // 사전 아이디 받아오기
-            axios.get(`https://kj273456.pythonanywhere.com/dictionary/id/${userId}`)
-            .then((res)=>{
-                dispatch(setDictionaryID({dictionaryId: res.data.data.id}));
-            }).catch((error)=>{
-                alert("사전 정보를 가져오지 못했습니다. 재로그인해주세요.");
-                navigate(-1);
-            });
         }).catch((error)=>{
             if(error.message=="로그인 실패"){
                 alert("아이디 또는 비밀번호를 확인해주세요.");
