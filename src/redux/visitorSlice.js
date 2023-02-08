bca6e169d7932f78088bddc633149bf82f27f9c8
gchaewon <conjs1@naver.com>
2023-02-08 22:58:36 +0900

    feat: 방문 userId 저장 리덕스 추가

--- src/redux/visitorSlice.js
+++ src/redux/visitorSlice.js
@@ -6,6 +6,7 @@ const name = 'VisitorSlice';
 const initialState = {
   nickname: '',// 방문자이름
   visit_dicName: '', // 방문한 사전이름
+  visit_userId: '', // 방문한 사전 유저아이디
 }
 
 export const visitorSlice = createSlice({
@@ -21,12 +22,15 @@ export const visitorSlice = createSlice({
     setVisit_dicName: (state, action) => {
       state.visit_dicName = action.payload.visit_dicName;
     },
+    setVisit_userId: (state,action) =>{
+      state.visit_userId = action.payload.visit_userId;
+    }
   },
     extraReducers: builder => {
       builder.addCase(PURGE, () => initialState);
     },
 });
 
-export const {setVisitor, initVisitor, setVisit_dicName} = visitorSlice.actions;
+export const {setVisitor, initVisitor, setVisit_dicName, setVisit_userId} = visitorSlice.actions;
 
 export default visitorSlice.reducer;
